bed3519f18d85c1c9c71d5dea22cc4ccd85a5fd6
Breno Azevedo <maddox.br@gmail.com>
2021-10-08 17:20:27 -0300

    Updated to work with Unity 2021.1, added [FindResourceOfType]
    * Re-added [FindResourceOfType] to the project
    * Removed UnityTestTools folder (not compatible with 2021.1 due to VS/mscorlib conflicts). The UnityTest scene might not work as a result.

---
+++ Scripts/Attributes/FindResourceOfTypeAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Syrinj.Attributes;
+
+namespace Syrinj
+{
+    public class FindResourceOfTypeAttribute : UnityConvenienceAttribute
+    {
+        public readonly Type ComponentType;
+
+        public FindResourceOfTypeAttribute()
+        { }
+
+        public FindResourceOfTypeAttribute(Type componentType)
+        {
+            ComponentType = componentType;
+        }
+    }
+}
---
+++ Scripts/Resolvers/FindResourceOfTypeResolver.cs
@@ -0,0 +1,42 @@
+﻿using Syrinj.Attributes;
+using Syrinj.Injection;
+using UnityEditor;
+using UnityEngine;
+
+namespace Syrinj.Resolvers
+{
+    public class FindResourceOfTypeResolver : IResolver
+    {
+        public object Resolve(Injectable injectable)
+        {
+            //FindObjectOfTypeAttribute
+            var attribute = (FindResourceOfTypeAttribute)injectable.Attribute;
+            if (attribute.ComponentType == null)
+            {
+                var resources = Resources.FindObjectsOfTypeAll(injectable.Type);
+                return resources?[0];
+            }
+            else
+            {
+                var resources = Resources.FindObjectsOfTypeAll(attribute.ComponentType);
+                return resources?[0];
+            }
+        }
+        // public static T[] GetAllInstances<T>() where T : ScriptableObject
+        // {
+        //     string[]
+        //         guids = AssetDatabase.FindAssets("t:" +
+        //                                          typeof(T)
+        //                                              .Name); //FindAssets uses tags check documentation for more info
+        //     T[] a = new T[guids.Length];
+        //     for (int i = 0; i < guids.Length; i++) //probably could get optimized 
+        //     {
+        //         string path = AssetDatabase.GUIDToAssetPath(guids[i]);
+        //         a[i] = AssetDatabase.LoadAssetAtPath<T>(path);
+        //     }
+        //
+        //     return a;
+        // }
+    }
+
+}
